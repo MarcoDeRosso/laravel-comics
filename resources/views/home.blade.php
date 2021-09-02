7d101d69b28146d47875dd7fa5cc11ea601abc9a
MarcoDeRosso <mcoder@hotmail.it>
2021-09-02 17:00:12 +0200

    route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,6 @@
 @section('title', 'HomePage')
 
 @section('content')
-
+ <h1>Qui ci vanno tutti i fumetti</h1>
+    <a href="{{route ('productpage')}}">CLICCA QUI!</a>
 @endsection
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,5 +3,6 @@
 @section('title', 'Product')
 
 @section('content')
-
+<h1>Qui ci va il singolo fumetto</h1>
+    <a href="{{route ('homepage')}}">CLICCA QUI!</a>
 @endsection
