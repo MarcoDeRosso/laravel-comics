f9a1bf48587cbd6c7f4dbb82a580f5440d6bb406
MarcoDeRosso <mcoder@hotmail.it>
2021-09-03 14:54:23 +0200

    comics homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,17 @@
 @section('title', 'HomePage')
 
 @section('content')
- <h1>Qui ci vanno tutti i fumetti</h1>
-    <a href="{{route ('productpage')}}">CLICCA QUI!</a>
+    <div class="container home-page-content">
+        <div class="row">
+            @foreach($comics as $item)
+                <div class="product-card col-2 px-15">
+                    <div
+                    class="img-container"
+                    style=" background-image: url('{{$item['thumb']}}');"
+                    ></div>
+                    <h3>{{ strtoupper($item['title'])}}</h3>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
