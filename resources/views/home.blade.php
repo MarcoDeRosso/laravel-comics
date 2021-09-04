eddddbd4c978523b97ccf30de38ea2ca31e89115
MarcoDeRosso <mcoder@hotmail.it>
2021-09-04 13:33:23 +0200

    foreach comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,18 @@
 @section('title', 'HomePage')
 
 @section('content')
-    <div class="container home-page-content">
-        <div class="row">
-            @foreach($comics as $item)
-                <div class="product-card col-custom-2 px-15">
-                    <a href="{{ route('productpage', [ 'id' => $loop->iteration ]) }}">
-                        <div class="img-container" style=" background-image: url('{{$item['thumb']}}');"></div>
-                        <h3>{{ strtoupper($item['title'])}}</h3>
-                    </a>
-                </div>
-            @endforeach
+    <div  class="black-bacground">
+        <div class="container home-page-content">
+            <div class="row">
+                @foreach($comics as $item)
+                    <div class="product-card col-custom-2 px-15">
+                        <a href="{{ route('productpage', [ 'id' => $loop->iteration ]) }}">
+                            <div class="img-container" style=" background-image: url('{{$item['thumb']}}');"></div>
+                            <h3>{{ strtoupper($item['title'])}}</h3>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,19 +3,41 @@
 @section('title', 'Product')
 
 @section('content')
-@dump($comics[$arrayIndex])
-<div class="container">
-    <div class="row">
-        <div class="col-6">
-            <h1>{{$comics[$arrayIndex]['title']}}</h1>
-            <div>
-                <span>U.S.Price:{{$comics[$arrayIndex]['price']}}</span>
-                <span>Check Availability</span>
+<div class="contaier-fluid product-main">
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h1>{{$comics[$arrayIndex]['title']}}</h1>
+                <div>
+                    <span>U.S.Price:{{$comics[$arrayIndex]['price']}}</span>
+                    <span>Check Availability</span>
+                </div>
+                <p>{{$comics[$arrayIndex]['description']}}</p>
             </div>
-            <p>{{$comics[$arrayIndex]['description']}}</p>
+            <div class="col-2">
+                <img src="{{$comics[$arrayIndex]['thumb']}}" alt="">
+            </div>
         </div>
-        <div class="col-6">
-            <img src="{{$comics[$arrayIndex]['thumb']}}" alt="">
+        <div class="row">
+            <div class="col-6">
+                <h4>Talent</h4>
+                <div class="d-flex">
+                    <h5>Art By:</h5>
+                    @foreach($comics [$arrayIndex]['artists'] as $item )
+                        <a href="">{{$item}}</a>
+                    @endforeach
+                </div>
+                <div class="d-flex">
+                    <h5>Written By:</h5>
+                    @foreach($comics [$arrayIndex]['writers'] as $item )
+                        <a href="">{{$item}}</a>
+                    @endforeach
+                </div>
+            </div>
+            <div class="col-6">
+
+            </div>
+
         </div>
     </div>
 </div>
